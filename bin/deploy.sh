9a8aa27c9179d42eb1750bea58cef224445e19bb
Arthur Moll <arth.moll@gmail.com>
2016-02-22 13:00:51 +0100

    ajoute le script de peloy

---
+++ bin/deploy.sh
@@ -0,0 +1,13 @@
+
+
+
+#php bin/console doctrine:database:drop --force
+#php bin/console doctrine:database:create
+
+php bin/console doctrine:schema:update --force
+
+
+php bin/console cache:clear
+php bin/console cache:clear -e prod
+
+chmod -R 777 var
